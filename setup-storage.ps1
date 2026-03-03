f053c8e8c96cc155dbe4f617a04f4d05eb821d37
Keyhan Azarjoo <Keyhan.Azarjoo@seamlessdigital.com>
2026-03-03 11:16:03 +0000

    test5

--- setup-storage.ps1
+++ setup-storage.ps1
@@ -1,10 +1,19 @@
 $localWindowsScript = Join-Path $PSScriptRoot "windows\setup-storage.ps1"
 $remoteWindowsScript = "https://raw.githubusercontent.com/keyhan-azarjoo/S3/main/windows/setup-storage.ps1"
 $downloadedWindowsScript = Join-Path $PSScriptRoot "windows-setup-storage.ps1"
+$downloadedModuleRoot = Join-Path $PSScriptRoot "modules"
 
 if (Test-Path $localWindowsScript) {
   & $localWindowsScript
   exit $LASTEXITCODE
+}
+
+if (Test-Path $downloadedWindowsScript) {
+  Remove-Item -Path $downloadedWindowsScript -Force -ErrorAction SilentlyContinue
+}
+
+if (Test-Path $downloadedModuleRoot) {
+  Remove-Item -Recurse -Force -Path $downloadedModuleRoot -ErrorAction SilentlyContinue
 }
 
 Invoke-WebRequest -Uri $remoteWindowsScript -OutFile $downloadedWindowsScript -UseBasicParsing
